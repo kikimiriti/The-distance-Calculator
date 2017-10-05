9402ef4c8ab4865d2586ec1a0dc63a7974dc2323
kikimiriti <32511125+kikimiriti@users.noreply.github.com>
2017-10-05 18:56:42 +0300

    Add files via upload

---
+++ kiki.py
@@ -0,0 +1,11 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+
+============== RESTART: C:/Users/CYBERLYNKADMIN/Desktop/ght.py ==============
+enter the speed (m/s)
+30
+enter the time taken (seconds)
+25
+speed 30 and time 25 is distance 1.2 m/s
+>>> 
